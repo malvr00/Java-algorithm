c37ef8f8131dc8779c9f5df731f517366a4f5ee0
조용일 <yedn1128@naver.com>
2023-08-28 15:54:57 +0900

    5-1. 올바른 괄호

--- stap1-5/a5-1/src/Main.java
+++ stap1-5/a5-1/src/Main.java
@@ -32,8 +32,20 @@ public class Main {
         return answer;
     }
 
-    public static int solution2(String str) {
-        int answer = -1;
+    public static String solution2(String str) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+
+        for (char x : str.toCharArray()) {
+            if (x == '(') {
+                stack.push(x);
+            } else {
+                if (stack.isEmpty()) {
+                    return "NO";
+                }
+                stack.pop();
+            }
+        }
 
         return answer;
     }
