a3cf54c4d95f7f2df5f6bc9689690321a66ae1fa
조용일 <yedn1128@naver.com>
2023-08-31 17:58:39 +0900

    5-7. 교육과정 설계

--- stap1-5/a5-7/src/Main.java
+++ stap1-5/a5-7/src/Main.java
@@ -2,19 +2,56 @@ import java.util.*;
 
 public class Main {
     /*
-        6. 공주구하기
+        7. 교육과정 설계
      */
 
-    public static int solution(int n, int k) {
-        int answer = 0;
-        HashMap<Integer, Integer> num = new HashMap<>();
+    public static String solution(String str1, String str2) {
+        String answer = "YES";
+        Queue<Character> q = new LinkedList<>();
+
+        for (char x : str1.toCharArray()) {
+            q.offer(x);
+        }
+
+        for (char x : str2.toCharArray()) {
+            Character peek = q.peek();
+            if (!q.contains(x)) {
+                continue;
+            }
+            if (x == peek) {
+                q.poll();
+            } else {
+                return "NO";
+            }
+        }
+
+        if (q.size() > 0) {
+            return "NO";
+        }
+
+
 
         return answer;
     }
 
-    public static int solution2(int n, int k) {
-        int answer = 0;
-        Queue<Integer> q = new LinkedList<>();
+    public static String solution2(String need, String plan) {
+        String answer = "YES";
+        Queue<Character> q = new LinkedList<>();
+        for (char x : need.toCharArray()) {
+            q.offer(x);
+        }
+        for (char x : plan.toCharArray()) {
+            if (q.contains(x)) {
+                if (x != q.poll()) {
+                    return "NO";
+                }
+            }
+        }
+
+        if (!q.isEmpty()) {
+            return "NO";
+        }
+
 
         return answer;
     }
@@ -22,9 +59,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
 
-        int n = kb.nextInt();
-        int k = kb.nextInt();
+        String str1 = kb.nextLine();
+        String str2 = kb.nextLine();
 
-        System.out.println(solution(n, k));
+        System.out.println(solution(str1, str2));
     }
 }
