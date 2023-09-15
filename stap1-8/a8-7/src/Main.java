91db996304a011da3d26c42f637fd56e589364f2
조용일 <yedn1128@naver.com>
2023-09-15 15:55:20 +0900

    8-7. 조합수 (메모이제이션)

--- stap1-8/a8-7/src/Main.java
+++ stap1-8/a8-7/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class Main {
+    /*
+        7. 조합수 (메모이제이션)
+     */
+
+    public static void solution(int n) {
+
+    }
+
+    static int[][] dy = new int[35][35];
+    public static int DFS(int n, int r){
+        if(dy[n][r] > 0) return dy[n][r];
+        if (n == r || r == 0) {
+            return 1;
+        } else {
+            return dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int r = kb.nextInt();
+
+        System.out.println(DFS(n, r));
+
     }
 }
