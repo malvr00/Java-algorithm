beafd587a8f3527a69d04b7b9a20ebd5b66bf7ad
조용일 <yedn1128@naver.com>
2023-09-27 18:07:18 +0900

    9-6 원더랜드 (최소 스패닝 트리 - 크루스칼 : Union&Find 이용)

--- stap1-9/a9-6/src/Main.java
+++ stap1-9/a9-6/src/Main.java
@@ -1,5 +1,76 @@
+import java.util.*;
+
+class Edge implements Comparable<Edge> {
+    public int v1;
+    public int v2;
+    public int cost;
+
+    Edge(int v1, int v2, int cost) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge ob) {
+        return this.cost - ob.cost;
+    }
+}
 public class Main {
+
+    /*
+        6. 원더랜드(최소 스패닝 트리 - 크루스칼 : Uion&Find 이용)
+     */
+    static int[] unf;
+
+    public static int Find(int v) {
+        if (v == unf[v]) {
+            return unf[v];
+        } else {
+            return unf[v] = Find(unf[v]);
+        }
+    }
+
+    public static void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
+        if (fa != fb) {
+            unf[fa] = fb;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+
+        unf = new int[n + 1];
+        ArrayList<Edge> arr = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            unf[i] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            int c = kb.nextInt();
+
+            arr.add(new Edge(a, b, c));
+        }
+
+        int answer = 0;
+        Collections.sort(arr);
+        for (Edge ob : arr) {
+            int fv1 = Find(ob.v1);
+            int fv2 = Find(ob.v2);
+            if (fv1 != fv2) {
+                answer += ob.cost;
+                Union(fv1, fv2);
+            }
+        }
+
+        System.out.println(answer);
+
     }
 }
