0dbb5a78da7c17b4be28132e2fc66c3e9186353d
조용일 <yedn1128@naver.com>
2023-11-08 10:55:55 +0900

    gitignore add' ;

---
+++ programmers/level2/stap1/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    /**
+     *  Level2.
+     *  문제: 요격 시스템
+     *  정답: 3
+     */
+
+    public static int solution(int[][] targets) {
+        int answer = 0;
+        
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{4, 5}, {4, 8}, {10, 14}, {11, 13}, {5, 12}, {3, 7}, {1, 4}};
+        solution(arr);
+    }
+}
