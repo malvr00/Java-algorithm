17f398480df96ae3039b0476f9803a5260f67270
조용일 <yedn1128@naver.com>
2023-11-08 11:34:53 +0900

    level2 - 요격 시스템 ( Greedy )

--- programmers/level2/stap1/src/Main.java
+++ programmers/level2/stap1/src/Main.java
@@ -4,18 +4,34 @@ public class Main {
 
     /**
      *  Level2.
-     *  문제: 요격 시스템
+     *  문제: 요격 시스템 ( Greedy )
      *  정답: 3
      */
 
     public static int solution(int[][] targets) {
         int answer = 0;
-        
+
+        int endPoint = Integer.MIN_VALUE;
+
+        Arrays.sort(targets, Comparator.comparingInt(o -> o[1]));
+
+        for (int i = 0; i < targets.length; i++) {
+            int s = targets[i][0];
+            int e = targets[i][1];
+            if(endPoint <= s){
+                answer++;
+                endPoint = e;
+            }
+        }
+
         return answer;
     }
 
     public static void main(String[] args) {
         int[][] arr = {{4, 5}, {4, 8}, {10, 14}, {11, 13}, {5, 12}, {3, 7}, {1, 4}};
-        solution(arr);
+
+        int answer = solution(arr);
+
+        System.out.println(answer);
     }
 }
