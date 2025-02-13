b9a63ea023546edfeb00eab0206fa2d31642c7fe
조용일 <yedn1128@naver.com>
2025-02-13 22:38:52 +0900

    Level 3 - 인사고과 로직 최적화 및 설명 README 추가

--- programmers/level3/step7/src/Main.java
+++ programmers/level3/step7/src/Main.java
@@ -7,100 +7,28 @@ public class Main {
      * 인사고과
      * Greedy
      */
-    static public class Score implements Comparable<Score> {
-        int aPoint;
-        int bPoint;
-        int sum;
-        int idx;
-
-        public Score(int aPoint, int bPoint, int idx) {
-            this.aPoint = aPoint;
-            this.bPoint = bPoint;
-            this.sum = aPoint + bPoint;
-            this.idx = idx;
-        }
-
-        @Override
-        public int compareTo(Score obj) {
-            if (obj.aPoint == this.aPoint) {
-                return obj.bPoint - this.bPoint;
-            }
-            return obj.aPoint - this.aPoint;
-        }
-    }
-
-    static public class SumScore implements Comparable<SumScore> {
-        int sum;
-        int idx;
-
-        public SumScore(int sum, int idx) {
-            this.sum = sum;
-            this.idx = idx;
-        }
-
-        @Override
-        public int compareTo(SumScore obj) {
-            if (obj.sum == this.sum) {
-                return this.idx - obj.idx;
-            }
-            return obj.sum - this.sum;
-        }
-    }
-
     public int solution(int[][] scores) {
-        if(scores.length == 1) return 1;
+        // [0] = 근무, [1] = 직원
+        int answer = 1;
+        int[] my = scores[0];
 
-        List<Score> list = new ArrayList<>();
-        for (int i = 0; i < scores.length; i++) {
-            list.add(new Score(scores[i][0], scores[i][1], i));
-        }
+        Arrays.sort(scores, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
 
-        Collections.sort(list);
-        List<SumScore> sumList = new ArrayList<>();
-        int aMax = list.get(0).aPoint;
-        int bMax = list.get(0).bPoint;
-        sumList.add(new SumScore(list.get(0).sum, list.get(0).idx));
-
-        int idx = 1;
-        for (; idx < list.size(); idx++) {
-            Score score = list.get(idx);
-            if(score.aPoint == aMax) {
-                sumList.add(new SumScore(score.sum, score.idx));
+        int max = 0;
+        for (int[] score : scores) {
+            // 직원 비교
+            if (max > score[1]) {
+                if (Arrays.equals(my, score)) {
+                    return -1;
+                }
             } else {
-                break;
+                if(my[0] + my[1] < score[0] + score[1]) answer++;
             }
+
+            max = Math.max(max, score[1]);
         }
 
-        int tempMax = bMax;
-        for (; idx < list.size(); idx++) {
-            boolean flag = false;
-            Score score = list.get(idx);
-            if (score.aPoint != list.get(idx - 1).aPoint) {
-                bMax = Math.max(bMax, tempMax);
-            }
-            tempMax = Math.max(tempMax, score.bPoint);
-            if (bMax > score.bPoint) {
-                if(score.idx == 0) return -1;
-                flag = true;
-            }
-            if(flag) continue;
-            sumList.add(new SumScore(score.sum, score.idx));
-        }
-
-        Collections.sort(sumList);
-
-        int cnt = 1;
-        if(sumList.get(0).idx == 0) return cnt;
-        int point = sumList.get(0).sum;
-        for (int i = 1; i < sumList.size(); i++) {
-            cnt++;
-            if (sumList.get(i).sum < point) {
-                point = sumList.get(i).sum;
-                if(sumList.get(i).idx == 0) return cnt;
-            }
-        }
-
-        return -1;
+        return answer;
     }
 
     public static void main(String[] args) {
